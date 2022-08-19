6a0e5244e1d0b3b273e9b2fa45603412d7e43aff
telegrambotbinacne <danya.garayan@gmail.com>
2022-08-19 17:35:29 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import collections
+
+T = int(input())
+
+peregovorki = []
+
+vershina = set()
+for i in range(T):
+    d = input()
+    for j in range(len(d) - 3):
+        cur = d[j] + d[j + 1] + d[j + 2]
+        cur_next = d[j + 1] + d[j + 2] + d[j + 3]
+        peregovorki.append([cur, cur_next])
+        vershina.add(cur)
+        vershina.add(cur_next)
+
+peregovorki.sort()
+
+c = collections.Counter()
+for x, x1 in peregovorki:
+    c[x, x1] += 1
+
+print(len(vershina))
+
+print(len(c))
+for p in c:
+    print(p[0], p[1], c.get(p))
